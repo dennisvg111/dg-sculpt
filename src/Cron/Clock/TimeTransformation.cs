f3b3ea9fd31852dbe09de5d0f10ad5b025277d38
dennisvg111 <dennisvg111@gmail.com>
2024-05-18 12:50:18 +0200

    Update transformation class

--- src/Cron/Clock/TimeTransformation.cs
+++ src/Cron/Clock/TimeTransformation.cs
@@ -1,66 +1,38 @@
-﻿using DG.Sculpt.Cron.Extensions;
-using System;
+﻿using System;
 
 namespace DG.Sculpt.Cron.Clock
 {
-    internal class TimeTransformation
+    internal abstract class TimeTransformation
     {
-        private readonly TimeSpan _modifier;
-        private readonly int _lowestValue;
-        private readonly Func<DateTimeOffset, int> _findCurrent;
         private readonly TimeTransformation _overflowTransformation;
 
-        public TimeTransformation(TimeSpan modifier, int lowestValue, Func<DateTimeOffset, int> findCurrent, TimeTransformation overflowTransformation)
+        public TimeTransformation(TimeTransformation overflowTransformation)
         {
-            _modifier = modifier;
-            _lowestValue = lowestValue;
-            _findCurrent = findCurrent;
             _overflowTransformation = overflowTransformation;
         }
 
-        public virtual DateTimeOffset Transform(DateTimeOffset time, int target, int direction = 1)
+        public DateTimeOffset Transform(DateTimeOffset time)
         {
-            var current = _findCurrent(time);
-            if (target != current && _overflowTransformation != null)
+            var result = MoveUntillValid(time);
+            if (!result.IsChanged || _overflowTransformation == null)
             {
-                time = _overflowTransformation.ResetToLowest(time);
-                current = _findCurrent(time);
+                return result.Result;
             }
-            while (current != target)
-            {
-                time = time.Add(_modifier.Multiply(direction));
-                current = _findCurrent(time);
-            }
-            return time;
+            return _overflowTransformation.Reset(result.Result);
         }
 
-        public virtual DateTimeOffset ResetToLowest(DateTimeOffset time)
+        private DateTimeOffset Reset(DateTimeOffset time)
         {
-            if (_findCurrent(time) == _lowestValue)
+            time = MoveBackwardsToLowest(time);
+            if (_overflowTransformation == null)
             {
                 return time;
             }
-            return Transform(time, _lowestValue, -1);
+            return _overflowTransformation.Reset(time);
         }
 
-        public static TimeTransformation ForMinutes(int lowestValue)
-        {
-            return new TimeTransformation(TimeSpan.FromMinutes(1), lowestValue, (t) => t.Minute, null);
-        }
+        public abstract TransformationResult MoveUntillValid(DateTimeOffset time);
 
-        public static TimeTransformation ForHours(int lowestValue, TimeTransformation minutesTransformation)
-        {
-            return new TimeTransformation(TimeSpan.FromHours(1), lowestValue, (t) => t.Hour, minutesTransformation);
-        }
-
-        public static TimeTransformation ForDays(int lowestValue, TimeTransformation hoursTransformation)
-        {
-            return new TimeTransformation(TimeSpan.FromHours(1), lowestValue, (t) => t.Day, hoursTransformation);
-        }
-
-        public static TimeTransformation ForMonths(int lowestValue, TimeTransformation daysTransformation)
-        {
-            return new TimeTransformation(TimeSpan.FromDays(1), lowestValue, (t) => t.Month, daysTransformation);
-        }
+        public abstract DateTimeOffset MoveBackwardsToLowest(DateTimeOffset time);
     }
 }
---
+++ src/Cron/Clock/TransformationResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DG.Sculpt.Cron.Clock
+{
+    internal class TransformationResult
+    {
+        private bool _isChanged;
+        private DateTimeOffset _result;
+
+        public bool IsChanged => _isChanged;
+        public DateTimeOffset Result => _result;
+
+        public TransformationResult(bool isChanged, DateTimeOffset result)
+        {
+            _isChanged = isChanged;
+            _result = result;
+        }
+    }
+}
